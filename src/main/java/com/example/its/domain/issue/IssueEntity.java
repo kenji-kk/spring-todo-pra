ff1074570ca73c4ad46dea986ae1531c79fe095b
kenji-kk <kenjix12x22@gmail.com>
2023-07-15 14:33:56 +0900

    create model,controller

--- src/main/java/com/example/its/domain/issue/IssueEntity.java
+++ src/main/java/com/example/its/domain/issue/IssueEntity.java
@@ -1,2 +1,37 @@
-package com.example.its.domain.issue;public class IssueEntity {
+package com.example.its.domain.issue;
+
+public class IssueEntity {
+    private long id;
+    private String summary;
+    private String description;
+
+    public IssueEntity(long id, String summary, String description) {
+        this.id = id;
+        this.summary = summary;
+        this.description = description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- src/main/java/com/example/its/web/issue/IssueController.java
+++ src/main/java/com/example/its/web/issue/IssueController.java
@@ -1,2 +1,25 @@
-package com.example.its.web.issue;public class IssueController {
+package com.example.its.web.issue;
+
+import com.example.its.domain.issue.IssueEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import java.util.Arrays;
+
+
+import java.util.List;
+
+@Controller
+public class IssueController {
+
+    @GetMapping("/issues")
+    public String showList(Model model) {
+        List<IssueEntity> issueList = Arrays.asList(
+                new IssueEntity(1,"概要1","説明1"),
+                new IssueEntity(2,"概要2","説明2"),
+                new IssueEntity(3,"概要3","説明3")
+        );
+        model.addAttribute("issueList", issueList);
+        return "issues/list";
+    }
 }
