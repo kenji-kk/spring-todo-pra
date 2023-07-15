098660caf170b91483b31cb946f01ea8f38d5631
kenji-kk <kenjix12x22@gmail.com>
2023-07-16 00:12:14 +0900

    create form

--- src/main/java/com/example/its/web/issue/IssueController.java
+++ src/main/java/com/example/its/web/issue/IssueController.java
@@ -6,20 +6,28 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import java.util.Arrays;
 
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/issues")
 @RequiredArgsConstructor
 public class IssueController {
 
     private final IssueService issueService;
 
-    @GetMapping("/issues")
+    @GetMapping
     public String showList(Model model) {
         model.addAttribute("issueList", issueService.findAll());
         return "issues/list";
+    }
+
+    @GetMapping("/creationForm")
+    public String showCreationForm() {
+        return "issues/creationForm";
     }
 }
